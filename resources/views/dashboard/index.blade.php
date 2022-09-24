670d1df8bb9e67a6566e13683db52275182eb7f8
TanzilalGummilang <tanzilalgummilang@gmail.com>
2022-09-24 22:50:05 +0700

    layout dashboard

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.master')
+@section('title')
+  Dashboard
+@endsection
+
+@section('main-content')
+  <div class="main-content">
+
+    <div class="content-wrapper">
+      <div class="row same-height">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header mt-2 mb-2">
+              <h4>@yield('title')</h4>
+            </div>
+            <div class="card-body mb-1">
+
+              <h5 class="mb-3 mt-1">Point of Sale</h5>
+              <h6 class="fw-normal">Aplikasi Transaksi Berbasis Web</h6>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+@endsection
